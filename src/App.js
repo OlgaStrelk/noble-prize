e168701e5dd6c0d7751617cc260c670c4f163d36
“Olga <strelod@yandex.ru>
2024-03-18 17:54:02 +0300

    feat: protected routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage, ListPage, CountryPage, PersonPage, NotFound404, LoginPage } from './pages';
-import {ProvideAuth} from "./services/auth";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  HomePage,
+  ListPage,
+  CountryPage,
+  PersonPage,
+  NotFound404,
+  LoginPage,
+} from "./pages";
+import { ProvideAuth } from "./services/auth";
+import { ProtectedRouteElement } from "./components/protected-route";
 
 export default function App() {
   return (
     <ProvideAuth>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/list/:country" element={<CountryPage />} />
-        <Route path="/list/:country/:personId" element={<PersonPage />} />
-        <Route path="*" element={<NotFound404 />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/list"
+            element={
+              <ProtectedRouteElement>
+                <ListPage />
+              </ProtectedRouteElement>
+            }
+          />
+          <Route
+            path="/list"
+            element={
+              <ProtectedRouteElement>
+                <CountryPage />
+              </ProtectedRouteElement>
+            }
+          />
+          <Route
+            path="/list"
+            element={
+              <ProtectedRouteElement>
+                <PersonPage />
+              </ProtectedRouteElement>
+            }
+          />
+          <Route path="*" element={<NotFound404 />} />
+        </Routes>
+      </BrowserRouter>
     </ProvideAuth>
   );
 }
--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,8 +1,8 @@
-import { useAuth } from '../services/auth';
-import { Navigate } from 'react-router-dom';
+import { useAuth } from "../services/auth";
+import { Navigate } from "react-router-dom";
 
 export const ProtectedRouteElement = ({ element }) => {
   let auth = useAuth();
 
-  return null; // здесь ваша реализация
-}
+  return auth.user ? element : <Navigate to="/login" replace />;
+};
