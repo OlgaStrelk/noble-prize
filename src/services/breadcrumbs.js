6e294eb6fa9f3f6935f858a00810933e8128e190
“Olga <strelod@yandex.ru>
2024-03-18 14:24:06 +0300

    feat: add auth

--- src/services/breadcrumbs.js
+++ src/services/breadcrumbs.js
@@ -1,7 +1,7 @@
 /*
  * state -- history state of app
  * route -- url of a new breadcrumb  */
-export const isContainRoute = (state, route) =>  state.some(({url}) => url === route);
+export const isContainRoute = (state, route) => state.some(({ url }) => url === route);
 
 /*
  * state -- history state of app
@@ -10,3 +10,5 @@ export const removeRemainingCrumbs = (state, url) => {
   const index = state.findIndex(({ url: route }) => route === url);
   return state.slice(0, index);
 };
+
+export const HOME_CRUMB = { path: '/', url: '/', title: 'Home' };
