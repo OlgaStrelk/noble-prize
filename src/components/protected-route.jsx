f728fafe6579d5470b2f1e45101fa9187120d4bc
“Olga <strelod@yandex.ru>
2024-03-19 17:08:41 +0300

    feat: auth check

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,16 +1,23 @@
-import { useAuth } from '../services/auth';
-import { Navigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useAuth } from "../services/auth";
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const ProtectedRouteElement = ({ element }) => {
   let { getUser, ...auth } = useAuth();
   const [isUserLoaded, setUserLoaded] = useState(false);
 
-  const init = async () => {};
+  const init = async () => {
+    await getUser();
+    setUserLoaded(true);
+  };
 
   useEffect(() => {
     init();
   }, []);
 
-  return auth.user ? element : <Navigate to="/login" replace/>;
-}
+  if (!isUserLoaded) {
+    return null;
+  }
+
+  return auth.user ? element : <Navigate to="/login" replace />;
+};
